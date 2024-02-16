eb4afa97d41408796c56b57f0ec20ba6bac73a84
Seif Moustafa <seif.moustafa516@gmail.com>
2024-02-16 17:13:18 +0200

    make logic

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -3,4 +3,18 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterCubit extends Cubit<CounterState> {
   CounterCubit(super.initialState);
+
+  int teamAPoints = 0;
+
+  int teamBPoints = 0;
+
+  void TeamIncreament({required String team, required int buttonNumber}) {
+    if (team == 'A') {
+      teamAPoints += buttonNumber;
+      emit(CounterAIncreamentState());
+    } else {
+      teamBPoints += buttonNumber;
+      emit(CounterBIncreamentState());
+    }
+  }
 }
